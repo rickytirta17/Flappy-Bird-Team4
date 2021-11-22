428bd188e5857a60662fdd44c689d561e405547f
Ricky Tirta Hardiyanta <83198877+rickytirta17@users.noreply.github.com>
2021-11-22 20:41:21 +0700

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas; 
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Time.timeScale = 1;
+        
+    }
+
+    // Update is called once per frame
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+        
+    }
+}
